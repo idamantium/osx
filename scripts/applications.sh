65ac5b4157ae13afbc46505810ae44b87e8a4be6
jmelvin <jmelvin@mobicorp.com>
2013-07-18 18:07:58 -0500

    removed a bunch of apps I don't want to install

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -10,9 +10,6 @@
 # Dropbox
 install_dmg_app "$DROPBOX_APP_URL" "$DROPBOX_APP_FILE" "Dropbox Installer" "$DROPBOX_APP_NAME"
 
-# Knox
-install_zip_app "$KNOX_APP_URL" "$KNOX_APP_FILE" "$KNOX_APP_NAME"
-
 # iTerm
 install_zip_app "$ITERM_APP_URL" "$ITERM_APP_FILE" "$ITERM_APP_NAME"
 
@@ -22,68 +19,20 @@ install_dmg_app "$SUBLIME_TEXT_APP_URL" "$SUBLIME_TEXT_APP_FILE" "Sublime Text 2
 # Alfred
 install_zip_app "$ALFRED_APP_URL" "$ALFRED_APP_FILE" "$ALFRED_APP_NAME"
 
-# TextExpander
-install_zip_app "$TEXTEXPANDER_APP_URL" "$TEXTEXPANDER_APP_FILE" "$TEXTEXPANDER_APP_NAME"
-
-# Path Finder
-install_zip_app "$PATH_FINDER_APP_URL" "$PATH_FINDER_APP_FILE" "$PATH_FINDER_APP_NAME"
-
 # Firefox
 install_dmg_app "$FIREFOX_APP_URL" "$FIREFOX_APP_FILE" "Firefox" "$FIREFOX_APP_NAME"
 
 # Google Chrome
 install_dmg_app "$CHROME_APP_URL" "$CHROME_APP_FILE" "Google Chrome" "$CHROME_APP_NAME"
 
-# Google Chrome Canary
-install_dmg_app "$CANARY_APP_URL" "$CANARY_APP_FILE" "Google Chrome Canary" "$CANARY_APP_NAME"
-
 # Opera
 install_dmg_app "$OPERA_APP_URL" "$OPERA_APP_FILE" "Opera" "$OPERA_APP_NAME"
-
-# OmniFocus
-install_dmg_app "$OMNIFOCUS_APP_URL" "$OMNIFOCUS_APP_FILE" "OmniFocus" "$OMNIFOCUS_APP_NAME"
-
-# OmniOutliner
-install_dmg_app "$OMNIOUTLINER_APP_URL" "$OMNIOUTLINER_APP_FILE" "OmniOutliner" "$OMNIOUTLINER_APP_NAME"
-
-# OmniGraffle
-install_dmg_app "$OMNIGRAFFLE_APP_URL" "$OMNIGRAFFLE_APP_FILE" "OmniGrafflePro" "$OMNIGRAFFLE_APP_NAME"
-
-# OmniDazzle
-install_dmg_app "$OMNIDAZZLE_APP_URL" "$OMNIDAZZLE_APP_FILE" "OmniDazzle" "$OMNIDAZZLE_APP_NAME"
-
-# Transmit
-install_zip_app "$TRANSMIT_APP_URL" "$TRANSMIT_APP_FILE" "$TRANSMIT_APP_NAME"
-
-# iStat Menus (the app is the installer)
-install_zip_app "$ISTATS_APP_URL" "$ISTATS_APP_FILE" "$ISTATS_APP_NAME"
-
-# Bartender
-install_zip_app "$BARTENDER_APP_URL" "$BARTENDER_APP_FILE" "$BARTENDER_APP_NAME"
 
 # Source Tree
 install_dmg_app "$SOURCE_TREE_APP_URL" "$SOURCE_TREE_APP_FILE" "SourceTree" "$SOURCE_TREE_APP_NAME"
 
 # GitHub
 install_zip_app "$GITHUB_APP_URL" "$GITHUB_APP_FILE" "$GITHUB_APP_NAME"
-
-# Snippets
-install_zip_app "$SNIPPETS_APP_URL" "$SNIPPETS_APP_FILE" "$SNIPPETS_APP_NAME"
-
-# Espresso
-install_zip_app "$ESPRESSO_APP_URL" "$ESPRESSO_APP_FILE" "$ESPRESSO_APP_NAME"
-
-# CodeKit
-install_zip_app "$CODEKIT_APP_URL" "$CODEKIT_APP_FILE" "$CODEKIT_APP_NAME"
-
-# Acorn
-install_zip_app "$ACORN_APP_URL" "$ACORN_APP_FILE" "$ACORN_APP_NAME"
-
-# DoubleTake
-install_dmg_app "$DOUBLETAKE_APP_URL" "$DOUBLETAKE_APP_FILE" "DoubleTake" "$DOUBLETAKE_APP_NAME"
-
-# ImageOptim
-install_tar_app "$IMAGE_OPTIM_APP_URL" "$IMAGE_OPTIM_APP_FILE" "-xjf" "$IMAGE_OPTIM_APP_NAME"
 
 # VLC
 install_dmg_app "$VLC_APP_URL" "$VLC_APP_FILE" "vlc-2.0.7" "$VLC_APP_NAME"
@@ -97,26 +46,11 @@ install_dmg_app "$PG_ADMIN_APP_URL" "$PG_ADMIN_APP_FILE" "pgAdmin3" "$PG_ADMIN_A
 # Sequel Pro
 install_dmg_app "$SEQUEL_PRO_APP_URL" "$SEQUEL_PRO_APP_FILE" "Sequel Pro 1.0.2" "$SEQUEL_PRO_APP_NAME"
 
-# Namebench
-install_dmg_app "$NAMEBENCH_APP_URL" "$NAMEBENCH_APP_FILE" "namebench" "$NAMEBENCH_APP_NAME"
-
-# Billings
-install_zip_app "$BILLINGS_APP_URL" "$BILLINGS_APP_FILE" "$BILLINGS_APP_NAME"
-
-# MoneyWell
-install_zip_app "$MONEYWELL_APP_URL" "$MONEYWELL_APP_FILE" "$MONEYWELL_APP_NAME"
-
 # Delicious Library
 install_zip_app "$DELICIOUS_LIBRARY_APP_URL" "$DELICIOUS_LIBRARY_APP_FILE" "$DELICIOUS_LIBRARY_APP_NAME"
 
 # CheatSheet
 install_zip_app "$CHEATSHEET_APP_URL" "$CHEATSHEET_APP_FILE" "$CHEATSHEET_APP_NAME"
-
-# OpenOffice
-install_dmg_app "$OPEN_OFFICE_APP_URL" "$OPEN_OFFICE_APP_FILE" "OpenOffice.org" "$OPEN_OFFICE_APP_NAME"
-
-# Cloak
-install_dmg_app "$CLOAK_APP_URL" "$CLOAK_APP_FILE" "Cloak" "$CLOAK_APP_NAME"
 
 # Screenhero
 install_dmg_app "$SCREENHERO_APP_URL" "$SCREENHERO_APP_FILE" "Screenhero" "$SCREENHERO_APP_NAME"
@@ -124,26 +58,8 @@ install_dmg_app "$SCREENHERO_APP_URL" "$SCREENHERO_APP_FILE" "Screenhero" "$SCRE
 # Colloquy
 install_zip_app "$COLLOQUY_APP_URL" "$COLLOQUY_APP_FILE" "$COLLOQUY_APP_NAME"
 
-# Wedge
-install_zip_app "$WEDGE_APP_URL" "$WEDGE_APP_FILE" "$WEDGE_APP_NAME"
-
 # Skype
 install_dmg_app "$SKYPE_APP_URL" "$SKYPE_APP_FILE" "Skype" "$SKYPE_APP_NAME"
 
 # VirtualBox
 install_dmg_pkg "$VIRTUAL_BOX_APP_URL" "$VIRTUAL_BOX_APP_FILE" "VirtualBox" "$VIRTUAL_BOX_APP_NAME"
-
-# Doxie
-install_dmg_app "$DOXIE_APP_URL" "$DOXIE_APP_FILE" "Doxie" "$DOXIE_APP_NAME"
-
-# Hazel (preference pane)
-install_dmg_app "$HAZEL_APP_URL" "$HAZEL_APP_FILE" "Hazel" "$HAZEL_APP_NAME"
-
-# Pastebot Sync (preference pane)
-install_dmg_app "$PASTEBOT_SYNC_APP_URL" "$PASTEBOT_SYNC_APP_FILE" "Pastebot Sync" "$PASTEBOT_SYNC_APP_NAME"
-
-# Air Display (preference pane)
-install_zip_pkg "$AIR_DISPLAY_APP_URL" "$AIR_DISPLAY_APP_FILE" "$AIR_DISPLAY_APP_NAME"
-
-# Eye-Fi
-install_dmg_pkg "$EYEFI_APP_URL" "$EYEFI_APP_FILE" "Eye-Fi Installer" "$EYEFI_APP_NAME"
--- settings/settings.sh
+++ settings/settings.sh
@@ -18,10 +18,6 @@ export DROPBOX_APP_URL=https://d1ilhw0800yew8.cloudfront.net/client
 export DROPBOX_APP_FILE=Dropbox%202.2.8.dmg
 export DROPBOX_APP_NAME=Dropbox.app
 
-export KNOX_APP_URL=https://d13itkw33a7sus.cloudfront.net/dist/K
-export KNOX_APP_FILE=Knox-2.2.0.zip
-export KNOX_APP_NAME=Knox.app
-
 export ITERM_APP_URL=https://iterm2.googlecode.com/files
 export ITERM_APP_FILE=iTerm2-1_0_0_20130319.zip
 export ITERM_APP_NAME=iTerm.app
@@ -98,10 +94,6 @@ export EMBER_INSPECTOR_EXTENSION_PATH="$CHROME_EXTENSION_ROOT/EmberInspector"
 export CHROME_LOGGER_EXTENSION_URL="git://github.com/ccampbell/chromelogger.git"
 export CHROME_LOGGER_EXTENSION_PATH="$CHROME_EXTENSION_ROOT/ChromeLogger"
 
-export CANARY_APP_URL=https://storage.googleapis.com/chrome-canary
-export CANARY_APP_FILE=GoogleChromeCanary.dmg
-export CANARY_APP_NAME="Google Chrome Canary.app"
-
 export FIREFOX_APP_URL=http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/22.0/mac/en-US
 export FIREFOX_APP_FILE=Firefox%2022.0.dmg
 export FIREFOX_APP_NAME=Firefox.app
@@ -110,34 +102,6 @@ export OPERA_APP_URL=http://get.geo.opera.com/pub/opera/desktop/15.0.1147.132/ma
 export OPERA_APP_FILE=Opera_15.0.1147.132_Setup.dmg
 export OPERA_APP_NAME=Opera.app
 
-export OMNIFOCUS_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIFOCUS_APP_FILE=OmniFocus-1.10.4.dmg
-export OMNIFOCUS_APP_NAME=OmniFocus.app
-
-export OMNIOUTLINER_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.4
-export OMNIOUTLINER_APP_FILE=OmniOutliner-3.10.6.dmg
-export OMNIOUTLINER_APP_NAME=OmniOutliner.app
-
-export OMNIGRAFFLE_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIGRAFFLE_APP_FILE=OmniGrafflePro-5.4.3.dmg
-export OMNIGRAFFLE_APP_NAME="OmniGraffle Professional 5.app"
-
-export OMNIDAZZLE_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIDAZZLE_APP_FILE=OmniDazzle-1.2.dmg
-export OMNIDAZZLE_APP_NAME=OmniDazzle.app
-
-export TRANSMIT_APP_URL=https://www.panic.com/transmit/d
-export TRANSMIT_APP_FILE=Transmit%204.4.zip
-export TRANSMIT_APP_NAME=Transmit.app
-
-export ISTATS_APP_URL=http://s3.amazonaws.com/bjango/files/istatmenus4
-export ISTATS_APP_FILE=istatmenus4.06.zip
-export ISTATS_APP_NAME="iStat Menus.app"
-
-export BARTENDER_APP_URL=http://www.macbartender.com/Demo
-export BARTENDER_APP_FILE=Bartender.zip
-export BARTENDER_APP_NAME=Bartender.app
-
 export SOURCE_TREE_APP_URL=http://downloads.atlassian.com/software/sourcetree
 export SOURCE_TREE_APP_FILE=SourceTree_1.6.1.dmg
 export SOURCE_TREE_APP_NAME=SourceTree.app
@@ -145,30 +109,6 @@ export SOURCE_TREE_APP_NAME=SourceTree.app
 export GITHUB_APP_URL=https://github-central.s3.amazonaws.com
 export GITHUB_APP_FILE=mac%2FGitHub%20for%20Mac%20155.zip
 export GITHUB_APP_NAME=GitHub.app
-
-export SNIPPETS_APP_URL=http://www.snippetsapp.com/download
-export SNIPPETS_APP_FILE=Snippets-1.4.1.zip
-export SNIPPETS_APP_NAME=Snippets.app
-
-export ESPRESSO_APP_URL=https://s3.amazonaws.com/macrabbit/downloads
-export ESPRESSO_APP_FILE=Espresso%202.1.zip
-export ESPRESSO_APP_NAME=Espresso.app
-
-export CODEKIT_APP_URL=http://incident57.com/codekit/files
-export CODEKIT_APP_FILE=codekit-8283.zip
-export CODEKIT_APP_NAME=CodeKit.app
-
-export ACORN_APP_URL=http://flyingmeat.com/download
-export ACORN_APP_FILE=Acorn.zip
-export ACORN_APP_NAME=Acorn.app
-
-export DOUBLETAKE_APP_URL=http://echoone.com/doubletake
-export DOUBLETAKE_APP_FILE=DoubleTake.dmg
-export DOUBLETAKE_APP_NAME=DoubleTake.app
-
-export IMAGE_OPTIM_APP_URL=http://imageoptim.com
-export IMAGE_OPTIM_APP_FILE=ImageOptim.tbz2
-export IMAGE_OPTIM_APP_NAME=ImageOptim.app
 
 export VLC_APP_URL=http://ftp.osuosl.org/pub/videolan/vlc/2.0.7/macosx
 export VLC_APP_FILE=vlc-2.0.7.dmg
@@ -186,18 +126,6 @@ export SEQUEL_PRO_APP_URL=https://sequel-pro.googlecode.com/files
 export SEQUEL_PRO_APP_FILE=sequel-pro-1.0.2.dmg
 export SEQUEL_PRO_APP_NAME="Sequel Pro.app"
 
-export NAMEBENCH_APP_URL=https://namebench.googlecode.com/files
-export NAMEBENCH_APP_FILE=namebench-1.3.1-Mac_OS_X.dmg
-export NAMEBENCH_APP_NAME=namebench.app
-
-export BILLINGS_APP_URL=http://download.marketcircle.com/billings
-export BILLINGS_APP_FILE=Billings376.zip
-export BILLINGS_APP_NAME=Billings.app
-
-export MONEYWELL_APP_URL=http://downloads.nothirst.com
-export MONEYWELL_APP_FILE=MoneyWell_17.zip
-export MONEYWELL_APP_NAME=MoneyWell.app
-
 export DELICIOUS_LIBRARY_APP_URL=http://www.delicious-monster.com/downloads
 export DELICIOUS_LIBRARY_APP_FILE=DeliciousLibrary3.zip
 export DELICIOUS_LIBRARY_APP_NAME="Delicious Library 3.app"
@@ -205,14 +133,6 @@ export DELICIOUS_LIBRARY_APP_NAME="Delicious Library 3.app"
 export CHEATSHEET_APP_URL=http://mediaatelier.com/CheatSheet
 export CHEATSHEET_APP_FILE=CheatSheet_1.1.1.zip
 export CHEATSHEET_APP_NAME=CheatSheet.app
-
-export OPEN_OFFICE_APP_URL=http://hivelocity.dl.sourceforge.net/project/openofficeorg.mirror/stable/3.4.1
-export OPEN_OFFICE_APP_FILE=Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg
-export OPEN_OFFICE_APP_NAME=OpenOffice.org.app
-
-export CLOAK_APP_URL=https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release
-export CLOAK_APP_FILE=Cloak-1.2.2.dmg
-export CLOAK_APP_NAME=Cloak.app
 
 export SCREENHERO_APP_URL=http://dl.screenhero.com/update/screenhero
 export SCREENHERO_APP_FILE=Screenhero.dmg
@@ -222,10 +142,6 @@ export COLLOQUY_APP_URL=http://colloquy.info/downloads
 export COLLOQUY_APP_FILE=colloquy-latest.zip
 export COLLOQUY_APP_NAME=Colloquy.app
 
-export WEDGE_APP_URL=http://wedge.natestedman.com/release
-export WEDGE_APP_FILE=Wedge.app.zip
-export WEDGE_APP_NAME=Wedge.app
-
 export SKYPE_APP_URL=http://download.skype.com/macosx
 export SKYPE_APP_FILE=Skype_6.6.0.467.dmg
 export SKYPE_APP_NAME=Skype.app
@@ -233,23 +149,3 @@ export SKYPE_APP_NAME=Skype.app
 export VIRTUAL_BOX_APP_URL=http://dlc.sun.com.edgesuite.net/virtualbox/4.2.12
 export VIRTUAL_BOX_APP_FILE=VirtualBox-4.2.12-84980-OSX.dmg
 export VIRTUAL_BOX_APP_NAME=VirtualBox.app
-
-export DOXIE_APP_URL=http://www.getdoxie.com/resources/files
-export DOXIE_APP_FILE=Doxie.dmg
-export DOXIE_APP_NAME=Doxie.app
-
-export HAZEL_APP_URL=https://s3.amazonaws.com/Noodlesoft
-export HAZEL_APP_FILE=Hazel-3.1.2.dmg
-export HAZEL_APP_NAME=Hazel.prefPane
-
-export PASTEBOT_SYNC_APP_URL=http://tapbots.net/pastebot
-export PASTEBOT_SYNC_APP_FILE=PastebotSync.dmg
-export PASTEBOT_SYNC_APP_NAME=PastebotSync.prefPane
-
-export AIR_DISPLAY_APP_URL=http://avatron.com/downloads
-export AIR_DISPLAY_APP_FILE=AirDisplayInstaller.zip
-export AIR_DISPLAY_APP_NAME="Air Display Preferences.prefPane"
-
-export EYEFI_APP_URL=http://support.eye.fi/agent/download/3.4.29
-export EYEFI_APP_FILE=Eye-Fi.dmg
-export EYEFI_APP_NAME="Eye-Fi/Eye-Fi Center.app"
